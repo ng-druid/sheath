b56f0c8a3511a9c9a5d7350e4a4bcfeba876f560
druid <angular.druid@gmail.com>
2022-03-20 11:11:08 -0400

    Resolve path to node.

--- src/stylize.js
+++ src/stylize.js
@@ -1,9 +1,20 @@
+import { getDiff } from 'recursive-diff';
+import domElementPath from 'dom-element-path';
+
 export default function stylize() {
   console.log('stylize');
+  const overlay = new Map();
 
   const observer = new MutationObserver((records) => {
     records.forEach(r => {
       console.log('mutation record', r);
+      // Could be part of rules engine -> pass changes through rules engine = much more flexible.
+      if (r.type === 'attributes' && r.attributeName === 'style' && r.target) {
+        const diff = getDiff(r.oldValue);
+        const path = domElementPath(r.target);
+        overlay.set(path, { ...(overlay.has(path) ? overlay.get(path)  : { [r.attributeName]: r.target[r.attributeName] })  });
+        console.log('overlay changed', overlay);
+      }
     });
   });
 
